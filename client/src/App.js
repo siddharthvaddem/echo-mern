628aa3c94b9ac4c504f98eb07ecc3111c61b9fab
siddharthvaddem <siddharthvaddem@yahoo.com>
2021-05-02 22:06:12 +0530

    create posts w filebug

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import {useDispatch} from 'react-redux'
+ 
+import {getPosts} from './actions/posts';
+import echoes from './images/echoes.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form' 
+import useStyles from './styles'
 
 const App = () => {
+    const classes=useStyles();
+    const dispatch=useDispatch();
+
+    useEffect(()=>{
+        dispatch(getPosts());
+    },[dispatch])
     return (
-        <div>
-            <h1>APP</h1>
-        </div>
+
+        <Container maxwidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <Typography className={classes.heading} variant="h2" align="center">ECHOES</Typography>
+        <img className={classes.image} src={echoes} alt="echoes" height="60"/>
+        </AppBar>
+        <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                    <Posts/>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form/>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+        </Container>
     )
 }
 
