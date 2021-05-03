711e50caec4f494e65f8bc3c16b5416b215e2437
siddharthvaddem <siddharthvaddem@yahoo.com>
2021-05-03 11:00:14 +0530

    cluster connection fixed

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ app.use(cors());
 app.use('/posts',postRoutes);
 
 
-const CONNECTION_URL="mongodb+srv://vaddem:vaddem123@cluster0.t1wnt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+ const CONNECTION_URL="mongodb+srv://testuser:testpassword@cluster0.hwwcr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const PORT=process.env.PORT||5000;
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
