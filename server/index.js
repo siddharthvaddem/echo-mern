1719edb6741859e0ca19c4c6a5130ccebdfec966
siddharthvaddem <siddharthvaddem@yahoo.com>
2021-05-08 15:40:03 +0530

    display posts as card

--- server/index.js
+++ server/index.js
@@ -15,8 +15,7 @@ app.use(cors());
 app.use('/posts',postRoutes);
 
 
- const CONNECTION_URL="mongodb+srv://testuser:testpassword@cluster0.hwwcr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
+const CONNECTION_URL="mongodb+srv://test:test@cluster0.ddrti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT=process.env.PORT||5000;
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
